96c305fe696c0f9b497d3f68ebf0414b6f3b15b2
Aman Lachhiramka <a-lachhiramka@ti.com>
2025-09-25 16:22:00 +0530

    drivers: pinctrl: Unify MSPM0 and MSPM33 pinctrl drivers
    This commit unifies the pinctrl drivers for TI's MSP microcontroller
    family by replacing the MSPM0-specific driver with a common MSP driver
    that supports both MSPM0 and MSPM33 series. The changes include:
    
    - Renamed driver files from mspm0-specific to generic msp names
    - Updated device tree bindings to support both microcontroller series
    - Created common pin function definitions in msp-pinctrl.h
    - Aligned pinctrl_soc.h implementations between MSPM0 and MSPM33
    - Updated device tree compatible string from "ti,mspm0-pinctrl" to
    "ti,msp-pinctrl"
    - Updated Kconfig to enable the driver for both microcontroller families
    
    Signed-off-by: Aman Lachhiramka <a-lachhiramka@ti.com>

---
+++ drivers/pinctrl/pinctrl_msp.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2025 Texas Instruments
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/pinctrl.h>
+#include <ti/driverlib/dl_gpio.h>
+
+/* Common driver for MSP family microcontrollers */
+#define DT_DRV_COMPAT ti_msp_pinctrl
+
+#define MSP_PINCM(pinmux)        ((pinmux & 0xFFFF0000) >> 16)
+#define MSP_PIN_FUNCTION(pinmux) (pinmux & 0x3F)
+
+int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt, uintptr_t reg)
+{
+	ARG_UNUSED(reg);
+
+	uint8_t pin_function;
+	uint32_t pin_cm;
+	uint32_t iomux;
+
+	for (int i = 0; i < pin_cnt; i++) {
+		pin_cm = MSP_PINCM(pins[i].pinmux);
+		pin_function = MSP_PIN_FUNCTION(pins[i].pinmux);
+		iomux = pins[i].iomux;
+
+		/* Check for invalid pull-up/pull-down configuration */
+		if (((iomux >> MSP_GPIO_RESISTOR_PULL_UP) & 0x1) &&
+		    ((iomux >> MSP_GPIO_RESISTOR_PULL_DOWN) & 0x1)) {
+			LOG_ERR("Pin CM%d: Cannot enable both pull-up and pull-down simultaneously",
+				pin_cm);
+			return -EINVAL;
+		}
+
+		if (pin_function == 0x00) {
+			DL_GPIO_initPeripheralAnalogFunction(pin_cm);
+		} else {
+			DL_GPIO_initPeripheralFunction(pin_cm, (iomux | pin_function));
+		}
+	}
+	return 0;
+}
